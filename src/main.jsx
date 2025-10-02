951874920c991c8c579a8f977ad309e762fcf907
Stranger261 <150508790+Stranger261@users.noreply.github.com>
2025-10-02 08:06:18 +0800

    updated aws

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 import { Toaster } from 'react-hot-toast';
 
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.js';
 Amplify.configure(awsExports);
 
 createRoot(document.getElementById('root')).render(
