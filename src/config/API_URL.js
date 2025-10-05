086fd757b365860a5a3ceb53594f054fa3725368
Stranger261 <rwild261@gmail.com>
2025-10-05 09:43:29 +0800

    fixed response

--- src/config/API_URL.js
+++ src/config/API_URL.js
@@ -1,7 +1,8 @@
-export const AUTH_SERVICE_BASE_URL =
-  //  'https://auth.health-ease-hospital.com';
-  'http://localhost:8000';
-export const PATIENT_SERVICE_BASE_URL = 'http://localhost:8001';
-// 'https://patient.health-ease-hospital.com';
-export const APPOINTMENT_SERVICE_BASE_URL = 'http://localhost:8002';
-// 'https://appointment.health-ease-hospital.com';
+export const AUTH_SERVICE_BASE_URL = 'https://auth.health-ease-hospital.com';
+// 'http://localhost:8000';
+export const PATIENT_SERVICE_BASE_URL =
+  //  'http://localhost:8001';
+  'https://patient.health-ease-hospital.com';
+export const APPOINTMENT_SERVICE_BASE_URL =
+  // 'http://localhost:8002';
+  'https://appointment.health-ease-hospital.com';
