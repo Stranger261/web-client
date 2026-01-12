be7dc09c929e1a53f8e5e0f1533ab694fbf1a3b7
Stranger261 <rwild261@gmail.com>
2026-01-13 04:30:26 +0800

    basic tab fixed

--- src/pages/Patient/features/PatientMedicalHistory.jsx
+++ src/pages/Patient/features/PatientMedicalHistory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import { FileText } from 'lucide-react';
-import Card, { CardHeader, CardBody } from '../../../components/ui/Card';
+import Card, { CardHeader, CardBody } from '../../../components/ui/card';
 import Pagination from '../../../components/ui/Pagination';
 
 import MedicalRecordsTable from '../components/MedicalHistory/MedicalRecordsTable';
